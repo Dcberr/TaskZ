f82d305590da6334d9ca40007611a094b97ae18a
Dcberr <khongcanbiet941612@gmail.com>
2026-06-13 15:43:41 +0700

    feat: initialize spring boot project skeleton

---
+++ src/main/java/dcberr/taskz/TaskzApplication.java
@@ -0,0 +1,13 @@
+package dcberr.taskz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dcberr/taskz/TaskzApplicationTests.java
@@ -0,0 +1,13 @@
+package dcberr.taskz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
